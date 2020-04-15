d2fe84c375ce222241b52a28480fac27a0f02c06
Hasnah Ngah <hasnah@Hasnahs-MacBook-Air.local>
2020-04-15 17:33:37 +0800

    using a search query

--- server.js
+++ server.js
@@ -2,20 +2,28 @@ var express = require('express');
 const mongoose = require('mongoose');
 
 var app = express();
-const db = mongoose.connect('mongodb://localhost/bookAPI',{ useUnifiedTopology: true,useNewUrlParser: true  } );
+const db = mongoose.connect('mongodb://localhost/bookAPI', { useUnifiedTopology: true, useNewUrlParser: true });
 const bookRouter = express.Router();
 var port = process.env.PORT || 3000;
 const Book = require('./models/bookModels');
 
 bookRouter.route('/books')
     .get((req, res) => {
-        Book.find((err, books) => {
+        const { query } = req;
+        Book.find(query, (err, books) => {
             if (err) {
                 return res.send(err);
             } else {
-               return res.json(books);
+                return res.json(books);
             }
         });
+        // Book.find((err, books) => {
+        //     if (err) {
+        //         return res.send(err);
+        //     } else {
+        //         return res.json(books);
+        //     }
+        // });
         // const response = { hello: 'This is my API' };
         // res.json(response);
     });
